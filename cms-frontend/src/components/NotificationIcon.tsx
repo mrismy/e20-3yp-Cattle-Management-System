464b0ae029508bc33561c76365b1534b621a1fb3
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-02-09 00:44:58 +0530

    Added a notoficatoin icon

---
+++ cms-frontend/src/components/NotificationIcon.tsx
@@ -0,0 +1,18 @@
+import { MdNotifications } from 'react-icons/md';
+
+const NotificationIcon = ({ count }: { count: number }) => {
+  return (
+    <div className="relative">
+      <MdNotifications className="w-9 h-9 text-gray-800 hover:text-red-500" />
+
+      {/* Notification Badge */}
+      {count > 0 && (
+        <span className="absolute -top-1.5 -right-2.5 bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">
+          {count}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default NotificationIcon;
