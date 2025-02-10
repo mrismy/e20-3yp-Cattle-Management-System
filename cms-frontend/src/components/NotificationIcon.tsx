9df24ace8455486fae2a0d87415a0762521ddc6c
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-02-10 11:52:18 +0530

    Added a notification dropdown

--- cms-frontend/src/components/NotificationIcon.tsx
+++ cms-frontend/src/components/NotificationIcon.tsx
@@ -5,7 +5,7 @@ const NotificationIcon = ({ count }: { count: number }) => {
     <div className="relative">
       <MdNotifications className="w-9 h-9 text-gray-800 hover:text-red-500" />
 
-      {/* Notification Badge */}
+      {/* Notification icon */}
       {count > 0 && (
         <span className="absolute -top-1.5 -right-2.5 bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">
           {count}
