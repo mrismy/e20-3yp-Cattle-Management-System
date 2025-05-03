6c1fd40e7a4a40206e451359373ac20adcc20333
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-05-03 14:04:32 +0530

    Added user model

---
+++ cms-backend/src/model/UserModel.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
