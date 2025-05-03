e98cdeddfab02bd53f15c660ab4d8a72d87c6775
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-05-03 22:49:21 +0530

    Validation of errors

--- cms-backend/src/controller/authController.ts
+++ cms-backend/src/controller/authController.ts
@@ -1,4 +1,38 @@
+import { error } from 'console';
 import User from '../model/UserModel';
+
+interface UserInterface {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+// handle errors
+const handleErrors = (err: any) => {
+  console.log(err.message, err.code);
+  let errors: UserInterface = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  };
+
+  // duplicate error code
+  if (err.code === 11000) {
+    errors.email = 'Email already registered';
+    return errors;
+  }
+
+  // validation of errors
+  if (err.message.includes('User validation failed')) {
+    Object.values(err.errors).forEach((properties: any) => {
+      const path = properties.path as keyof UserInterface;
+      errors[path] = properties.message;
+    });
+  }
+  return errors;
+};
 
 module.exports.login = async (req: any, res: any) => {
   const { email, password } = req.body;
@@ -13,7 +47,8 @@ module.exports.login = async (req: any, res: any) => {
     res.status(200).json({ message: 'Login successful', user });
   } catch (error: any) {
     console.log(error);
-    res.status(500).json({ message: 'Error logging in', error: error.message });
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
   }
 };
 
@@ -21,20 +56,13 @@ module.exports.signup = async (req: any, res: any) => {
   const { firstName, lastName, email, password } = req.body;
 
   try {
-    // Check if user already exists
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
     const newUser = new User({ firstName, lastName, email, password });
-    newUser.save();
+    await newUser.save();
     res
       .status(201)
       .json({ message: 'User created successfully', user: newUser });
   } catch (error: any) {
-    console.log(error);
-    res
-      .status(500)
-      .json({ message: 'Error creating user', error: error.message });
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
   }
 };
--- cms-backend/src/model/UserModel.ts
+++ cms-backend/src/model/UserModel.ts
@@ -1,23 +1,25 @@
 import mongoose from 'mongoose';
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true,
+    required: [true, 'Please enter your first name'],
   },
   lastName: {
     type: String,
-    required: true,
+    required: [true, 'Please enter your last name'],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter your email'],
     unique: true,
+    validate: [isEmail, 'Please enter a valid email address'],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, 'Please enter your password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   },
 });
 
