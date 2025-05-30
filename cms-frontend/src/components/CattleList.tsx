7eaf4c45ca6b89be7c1886487e66edfb161470a6
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-05-30 09:46:54 +0530

    Changed ui for the cattle list. Filter cattle data according to their status

--- cms-frontend/src/components/CattleList.tsx
+++ cms-frontend/src/components/CattleList.tsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
 import { RiAddCircleLine } from 'react-icons/ri';
-import { getAllCattle } from '../services/CattleListService';
 import GlobalContext from '../context/GlobalContext';
 import AddCattleForm from './AddCattleForm';
 import { CattleData } from './Interface';
@@ -18,10 +17,14 @@ const CattleList = () => {
     showCattleCard,
     setShowCattleCard,
   } = useContext(GlobalContext);
-  const cattleStatus = ['all livestocks', 'safe', 'unsafe'];
+  const cattleStatus = ['all cattle', 'safe', 'alert', 'unsafe'];
+  const [selectedOption, setSelectedOption] = useState('all cattle');
   const [allCattleData, setAllCattleData] = useState<CattleData[]>([]);
   const [selectedCattleData, setSelectedCattleData] =
     useState<CattleData | null>(null);
+  const [filteredCattleData, setFilteredCattleData] = useState<CattleData[]>(
+    []
+  );
 
   const displayCattleCard = (cattleData: CattleData) => {
     setSelectedCattleData(cattleData);
@@ -34,6 +37,7 @@ const CattleList = () => {
   };
 
   const axiosPrivate = UseAxiosPrivate();
+
   // Fetch all cattle data
   const fetchAllCattle = async () => {
     try {
@@ -46,19 +50,38 @@ const CattleList = () => {
       setAllCattleData([]);
     }
   };
+
+  // Filter cattle based on active filter
+  const filterCattle = () => {
+    if (selectedOption === 'all cattle') {
+      setFilteredCattleData(allCattleData);
+    } else {
+      setFilteredCattleData(
+        allCattleData.filter(
+          (cattle) => cattle.status.toLowerCase() === selectedOption
+        )
+      );
+    }
+  };
+
   useEffect(() => {
     fetchAllCattle();
-  }, []);
+    filterCattle();
+  }, [selectedOption]);
 
   return (
     <div className="mt-12 overflow-x-auto px-5">
       <div className="flex items-start justify-between mb">
         {/* Navigation to display the livestocks with different status */}
-        <NavSub options={cattleStatus} />
+        <NavSub
+          options={cattleStatus}
+          selectedOption={selectedOption}
+          onSelect={setSelectedOption}
+        />
 
         {/* Add livestock button */}
         <button
-          className="flex items-center justify-center space-x-2 bg-green-700 py-2 px-4 rounded-md text-white text-sm font-medium hover:bg-white hover:text-green-800 hover:border-1 border-green-600"
+          className="flex items-center justify-center space-x-2 bg-green-600 hover:bg-green-700 py-2 px-4 rounded-md text-white text-sm font-medium transition-colors duration-200 shadow-sm w-full sm:w-auto"
           onClick={addCattleForm}>
           <div className="text-lg">
             <RiAddCircleLine />
@@ -69,8 +92,17 @@ const CattleList = () => {
       <hr className="text-gray-300 w-full mb-8" />
 
       {/* Table to display the livestock details */}
-      <table className="w-full">
-        <thead className="bg-green-700">
+      <table className="w-full divide-y divide-gray-200 rounded-lg overflow-hidden shadow-md">
+        <thead
+          className={`${
+            selectedOption === 'all cattle'
+              ? 'bg-gray-700'
+              : selectedOption === 'safe'
+              ? 'bg-green-700'
+              : selectedOption === 'alert'
+              ? 'bg-amber-700'
+              : 'bg-red-700'
+          }`}>
           <tr>
             {[
               'cattle id',
@@ -82,49 +114,73 @@ const CattleList = () => {
             ].map((heading) => (
               <th
                 key={heading}
-                className="text-white px-5 py-3 text-sm font-bold uppercase">
-                {heading.toLocaleUpperCase()}
+                className="py-4 text-center text-sm font-medium text-white uppercase tracking-wider">
+                {heading}
               </th>
             ))}
           </tr>
         </thead>
 
-        <tbody className="divide-y divide-white">
-          {allCattleData
-            .sort((a, b) => Number(a.cattleId) - Number(b.cattleId))
-            .map((cattleData: CattleData) => (
-              <tr
-                key={cattleData.cattleId}
-                onClick={() => displayCattleCard(cattleData)}
-                className="bg-green-50 hover:bg-green-100 items-center justify-center">
-                <td className="px-5 py-2 text-sm text-gray-700 text-center">
-                  {cattleData.cattleId}
-                </td>
-                <td className="px-5 py-2 text-sm text-gray-700 text-center">
-                  {cattleData.cattleName}
-                </td>
-                <td className="px-5 py-2 text-sm text-gray-700 text-center">
-                  {dayjs(cattleData.createdAt).format('DD/MM/YYYY HH:mm')}
-                </td>
-                <td className="px-5 py-2 text-sm text-gray-700 text-center">
-                  <div
-                    className={`${
-                      cattleData.status === 'safe'
-                        ? 'bg-green-200'
-                        : 'bg-red-200'
-                    } rounded-md`}>
-                    {cattleData.status}
-                  </div>
-                </td>
-                <td className="px-5 py-2 text-sm text-gray-700 text-center">
-                  {dayjs(cattleData.updatedAt).format('DD/MM/YYYY HH:mm')}
-                </td>
-                <td className="flex justify-center space-x-3 px-5 py-2 text-lg text-gray-700 text-center">
-                  <MdDeleteOutline />
-                  <MdOutlineEdit />
-                </td>
-              </tr>
-            ))}
+        <tbody className="bg-white divide-y divide-gray-100">
+          {filteredCattleData.length > 0 ? (
+            filteredCattleData
+              .sort((a, b) => Number(a.cattleId) - Number(b.cattleId))
+              .map((cattleData: CattleData) => (
+                <tr
+                  key={cattleData.cattleId}
+                  onClick={() => displayCattleCard(cattleData)}
+                  className="hover:bg-gray-50 cursor-pointer transition-colors">
+                  <td className="py-3 text-center text-sm font-medium text-gray-900">
+                    {cattleData.cattleId}
+                  </td>
+
+                  <td className="py-3 text-center text-sm text-gray-900">
+                    {cattleData.cattleName}
+                  </td>
+
+                  <td className="py-3 text-center text-sm text-gray-500">
+                    {dayjs(cattleData.createdAt).format('MMM D, YYYY h:mm A')}
+                  </td>
+
+                  <td className="py-3 text-center ">
+                    <span
+                      className={`px-2 py-1 inline-flex text-xs leading-5 font-semibold rounded-full ${
+                        cattleData.status === 'safe'
+                          ? 'bg-green-100 text-green-800'
+                          : cattleData.status === 'alert'
+                          ? 'bg-yellow-100 text-yellow-800'
+                          : 'bg-red-100 text-red-800'
+                      }`}>
+                      {cattleData.status.charAt(0).toUpperCase() +
+                        cattleData.status.slice(1)}
+                    </span>
+                  </td>
+
+                  <td className="py-3 text-center text-sm text-gray-500">
+                    {dayjs(cattleData.updatedAt).format('MMM D, YYYY h:mm A')}
+                  </td>
+
+                  <td className="py-3 text-sm font-medium ">
+                    <div className="flex justify-center space-x-3">
+                      <button className="text-blue-600 hover:bg-blue-600 hover:text-white rounded-full p-1">
+                        <MdOutlineEdit className="text-lg" />
+                      </button>
+                      <button className="text-red-600 hover:bg-red-600 hover:text-white rounded-full p-1">
+                        <MdDeleteOutline className="text-lg" />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+          ) : (
+            <tr>
+              <td
+                colSpan={6}
+                className="px-6 py-3 text-center text-sm text-gray-500">
+                No cattle are in {selectedOption.toUpperCase()} region
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
 
