1e61d4a0503900257d690dbad6987eb54e54ba97
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-07-09 20:54:38 +0530

    Fixed the error in the map marker

--- cms-frontend/src/components/GeoFence/GeoFenceMap.tsx
+++ cms-frontend/src/components/GeoFence/GeoFenceMap.tsx
@@ -2,6 +2,17 @@ import { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import Axios from '../../services/Axios';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 interface GeoFenceMapInterface {
   newLocation: boolean;
